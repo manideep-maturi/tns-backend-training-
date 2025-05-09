41fbe0d9fd6f2f743d6380613ac95e8292a9fd57
manideep-maturi <maturimanideep@gmail.com>
2025-05-09 12:40:31 +0530

    exception program

---
+++ backendtraining/src/com/tns/exceptionhandling/ExceptionDemo.java
@@ -0,0 +1,18 @@
+package com.tns.exceptionhandling;
+
+public class ExceptionDemo{
+	
+	
+	public static void main(String X []) {
+		int a=15;
+		int b=0;
+		float result;
+		try {
+			result=a/b;
+		}
+		catch(Exception e){
+			System.out.println("hello mani");
+		}
+	}
+	
+}
