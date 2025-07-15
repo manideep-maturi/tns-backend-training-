95050f5bb1b72ae2fd31483d568316430a10ac0e
MATURI MANIDEEP <163424658+manideep-maturi@users.noreply.github.com>
2025-07-15 19:37:15 +0530

    Add files via upload

---
+++ backendtraining/src/com/tns/exceptionhandling/TypesOfVariables.java
@@ -0,0 +1,10 @@
+package com.tns.day1;
+
+public class TypesOfVariables {
+int a=20;
+static int b=50;
+void print() {
+	int c=15;
+	System.out.println(c);
+}
+}
---
+++ backendtraining/src/com/tns/exceptionhandling/TypesOfVariablesMain.java
@@ -0,0 +1,11 @@
+package com.tns.day1;
+
+public class HelloMain {
+	public static void main(String[] args) {
+		Hello h=new Hello();
+		System.out.println(h.a);
+		System.out.println(h.b);
+		h.print();
+	}
+
+}
